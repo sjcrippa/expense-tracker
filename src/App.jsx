ecd86d7f9bbfb5dcdfe551cdd54045cc889829e7
Santiago Crippa <santicrippa@gmail.com>
2023-05-19 17:50:58 -0300

    income and expense operations

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { GlobalProvider } from './context/GlobalState';
 
-import { GlobalProvider } from './context/GlobalState'
-import Header from './components/Header'
-import Balance from './components/Balance'
-import TransactionForm from './components/transactions/TransactionForm'
-import TransactionList from './components/transactions/TransactionList'
+import Header from './components/Header';
+import Balance from './components/Balance';
+import TransactionForm from './components/transactions/TransactionForm';
+import TransactionList from './components/transactions/TransactionList';
+import IncomeExpenses from './components/IncomeExpenses';
 
 const App = () => {
 	return (
 		<GlobalProvider>
 			<Header />
+			<IncomeExpenses />
 			<Balance />
 			<TransactionForm />
 			<TransactionList />
